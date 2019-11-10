d1c09e5df23d2ebdfc9828186a9e158859b82ad0
Jacob Stoebel <jstoebel@gmail.com>
2019-11-10 11:17:40 -0500

    demo of form status

--- src/MyForm.tsx
+++ src/MyForm.tsx
@@ -4,25 +4,32 @@ import validatesPresense from './validations/validates-presence'
 
 const MyForm: React.FC = () => {
 
-  const {handleOnSubmit, handleFieldChange, fields} = useForm(
+  const {handleOnSubmit, handleFieldChange, fields, status} = useForm(
     {
       name: {
         value: '',
         submitValidations: [validatesPresense()],
-        changeValidations: [validatesPresense()]
+        changeValidations: []
       },
       email: {
         value: '',
         submitValidations: [validatesPresense()],
-        changeValidations: [validatesPresense()]
+        changeValidations: []
       },
     },
-    (e, fields) => {
+    (e, fields, resolve) => {
       console.log('hello from consumer submit callback!', fields);
+      console.log('pretending to wait for server');
+      
+      setTimeout(() => {
+        console.log('bad response from server!');
+        resolve(true)
+      }, 2000);
     }
   );
   return (
     <form onSubmit={handleOnSubmit}>
+      <h1>{`Form status: ${status}`}</h1>
       <div>
         <label>Name:</label>
         <input type="text" name="name" value={fields.name.value} onChange={handleFieldChange} />
